d053bc66c852d61ddcbed2087e17fdedea90d7f6
Setnea <gromadka.agnieszka@gmail.com>
2016-02-29 15:01:27 +0100

    Editing heart.py
    Editing heart.py: removing unnecessary code, adding proper logging
    settings

--- python/heart.py
+++ python/heart.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 import modules.file_managment as FM
 import scripts.editdata as ED
+import logging.config
 
 import logging
 import argparse
@@ -13,7 +14,7 @@ class Utilities():
     def moving_data(self, in_path, out_path, sleep_time, file_list, que):
         for i in range(len(file_list)):
             try:
-                out_path = FM.copy_data_const(file_list[i], out_path, sleep_time) # FM = file_managment module
+                out_path = FM.copy_data_constantly(file_list[i], out_path, sleep_time) # FM = file_managment module
                 que.put(out_path)
             except IOError as e:
                 print('Error: %s' % e.strerror)
@@ -24,44 +25,41 @@ class Utilities():
             out_path = "12"
             b = ED.example(out_path)
             print(b)  
-            
-    def create_logfile(self, log_filename):
-        output_path = os.path.dirname(os.path.realpath(__file__)) + "/logs/" + log_filename
-        FM.copy_data("logfile_temp", output_path)
 
 def main():
-  
+"""
+Logging section.
+"""
     log_filename = ((time.strftime("%Y_%m_%d_")  + "_" + time.strftime("%H_%M")) ) + ".log" # setup log filename
-# Logs settings (+ example log)
-    logging.basicConfig(level=logging.DEBUG, 
-		    filename="logfile_temp", 
-		    format="%(asctime)-2s \t %(levelname)-4s \t %(message)s") #logs' settings
-    logging.info("Starting program...")
-
-#Arg parse section
+    logging.config.fileConfig('logging.conf', defaults={'logfilename': log_filename}, disable_existing_loggers=False) #disable_existing_loggers=True, putting name of every module in logging.conf would be needed, False if we gonna make it unnecesarry
+    logger = logging.getLogger(__name__)
+    logger.info("Starting program...")
+"""
+Arg parse section.
+"""
     parser = argparse.ArgumentParser( description='\n (...Example description...)' )
     parser.add_argument( '-i',
-		metavar='<inputpath>', 
-		type=str, 
-		required=True, 
-		nargs=1, 
-		help='Input path' )
+                metavar='<inputpath>', 
+                type=str, 
+                required=True, 
+                nargs=1, 
+                help='Input path' )
     parser.add_argument( '-o',
-		metavar='<outputpath>', 
-		type=str, 
-		required=True, 
-		nargs=1, 
-		help='Output path' )
+                metavar='<outputpath>', 
+                type=str, 
+                required=True, 
+                nargs=1, 
+                help='Output path' )
     parser.add_argument( '-st',
-		metavar='<sleeptime>',  
-		default='3', #default sleep time [stop working somehow]
+                metavar='<sleeptime>',  
+                default='3', #default sleep time []
                 type=int,
                 nargs=1,
-		help='Sleep time' )
+                help='Sleep time' )
 
     args = parser.parse_args()
-# Program 
-    file_list = ["123.txt", "12.txt", "proba.txt"]
+# Program
+    file_list = []
     u = Utilities()
     q = multiprocessing.Queue()
   #  m = multiprocessing.Manager()
@@ -74,8 +72,6 @@ def main():
     p2.start()
     p1.join()
     p2.join()
-    u.create_logfile(log_filename) # creating final logfile placeholder
-
 
 
 if __name__ == "__main__":
