b137ccdc325f9e3a0fecafb5f9501571b03f16c3
Setnea <gromadka.agnieszka@gmail.com>
2016-03-18 07:24:54 +0100

    Editing queue module and Workflow Engine
    Adding multitasking option to queue (pool).
    Adding calling task executes to WorkflowEngine

--- python/WorkflowEngine.py
+++ python/WorkflowEngine.py
@@ -2,6 +2,8 @@
 import logging, multiprocessing, time, os
 import modules.logs_configuration as LC
 import modules.xml_parser as p
+from modules.queue import multiproc 
+from modules.task import TASK as tk
 #import logging.config
 
 
@@ -10,10 +12,16 @@ def main():
     LC.configure()
     logger = logging.getLogger(__name__)
     
+    dict_global = {}
     logger.info("Starting program...")
     input_path = "C://Users//Agnieszka//Documents//GitHub//PathwayPackage//input_output//input_settings.xml"
-    config_dict, task_list, task_list_para = p.parse(input_path)
-    print(config_dict)
+    #config_dict, task_list, task_list_para = p.parse(input_path)
+    cofig_dict, task_list = p.parse(input_path)
+    for element in task_list:
+        if isinstance(task, list):
+            dict_global = multiproc(dict_global, element, settings_dict)
+        else:
+            dict_global = element.tk.execute(dict_global)
     
 if __name__ == "__main__":
     main()
--- python/modules/queue.py
+++ python/modules/queue.py
@@ -1,7 +1,22 @@
 #! /usr/bin/python
+import modules.task as tk
+from multiprocessing import Pool
+#from functools import partial
+
 
 class QUEUE():
     
-    def execute(self, dict_global, task_list): # task_list passing by _init_ or function argument?
+    def execute(self, task, dict_global): 
+        #task_name = (type(task).__name__)
+        task.tk.execute(dict_global) # or executing = getattr(tk, "execute"), executing()
+
+            
+    def multiproc(self, dict_global, task_list, settings_dict):
+        cores = settings_dict["number_of_cores"]
+        #pool = Pool(processess = cores)
+        pool = Pool(processess = 2)
         for task in task_list:
-            task.TASK.execute(dict_global)
+            pool.apply_async(self.execute, (task, dict_global),) #partial_execute = partial(self.execute, dict_global=dict_global), apply_async(partial_execute, task)
+        pool.close()
+        pool.join()
+        return dict_global
