a470f2127f9df174885dc484e74c8ddc2f4866f6
Setnea <gromadka.agnieszka@gmail.com>
2016-03-21 12:59:02 +0100

    [DEBUG] logs, input_settings paths

--- python/WorkflowEngine.py
+++ python/WorkflowEngine.py
@@ -2,26 +2,27 @@
 import logging, multiprocessing, time, os
 import modules.logs_configuration as LC
 import modules.xml_parser as p
-from modules.queue import multiproc 
-from modules.task import TASK as tk
+from modules.queue import QUEUE
+from modules.task import TASK
 #import logging.config
 
 
 def main():
     
-    LC.configure()
+    PP_path = os.path.abspath('..')
+    LC.configure(PP_path)
     logger = logging.getLogger(__name__)
-    
     dict_global = {}
     logger.info("Starting program...")
-    input_path = "C://Users//Agnieszka//Documents//GitHub//PathwayPackage//input_output//input_settings.xml"
-    #config_dict, task_list, task_list_para = p.parse(input_path)
-    cofig_dict, task_list = p.parse(input_path)
+    input_path= (os.path.join(PP_path, "input_output", "input_settings.xml")).replace("\\", "//") # setting up path to input settings (in xml format)
+    config_dict, task_list, task_list_para = p.parse(input_path)
+    print(config_dict)
+    """cofig_dict, task_list = p.parse(input_path)
     for element in task_list:
         if isinstance(task, list):
-            dict_global = multiproc(dict_global, element, settings_dict)
+            dict_global = QUEUE.multiproc(dict_global, element, settings_dict)
         else:
-            dict_global = element.tk.execute(dict_global)
+            dict_global = element.TASK.execute(dict_global)"""
     
 if __name__ == "__main__":
     main()
--- python/modules/logs_configuration.py
+++ python/modules/logs_configuration.py
@@ -2,9 +2,10 @@
 import logging
 from time import strftime
 import logging.config
+import os, sys
 
-def configure():
-    log_output_path = "C://Users//Agnieszka//Documents//GitHub//PathwayPackage//python//logs//"
+def configure(PP_path):
+    log_output_path = (os.path.join(PP_path, "python", "logs", "")).replace("\\", "//")
     log_filename =  log_output_path + ((strftime("%Y_%m_%d_")  + "_" + strftime("%H_%M")) ) + ".log" # setup log filename
     logging.config.fileConfig('logging.conf', defaults={'logfilename': log_filename}, disable_existing_loggers=False) #disable_existing_loggers=True, putting name of every module in logging.conf would be needed, False if we gonna make it unnecesarry
     logger = logging.getLogger(__name__)
