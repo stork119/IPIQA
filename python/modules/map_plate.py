ee7c64cf30c33225174229f2fb02679d71eaa3f1
Setnea <gromadka.agnieszka@gmail.com>
2016-05-19 14:58:42 +0200

    [REWORK] #9

--- python/modules/map_plate.py
+++ python/modules/map_plate.py
@@ -3,30 +3,26 @@ import string, os
 import modules.file_managment as FM
 import csv
 
-def writing_output_csv(input_path, output_path, output_data, names):
-    out_file= open(output_path, "a")
+def preparing_output_csv(input_data, output_path, mp_data, names):
+    output_data = []
     num = 0
     position = 0 
-    for line in open(input_path):
+    for line in input_data:
         if num == 0: 
-            new_line = [line.rstrip()]
-            data = line.rstrip().split(",")
-            for i in range(len(data)):
-                if data[i] == "PositionName":
+            new_line = line
+            for i in range(len(line)):
+                if line[i] == "PositionName":
                     position = i
             for j in names:
                 new_line.append(j)
-            new_line = ",".join(new_line)
-            out_file.write(new_line + "\n")
+            output_data.append(new_line)
             num = 1
         else:
-            new_line = [line.rstrip()]
-            data = line.rstrip().split(",")
-            key = data[position]           
-            new_line.append(output_data[key])
-            new_line = ",".join(new_line)
-            out_file.write(new_line + "\n")
-    out_file.close()
+            new_line = line
+            key = line[position]           
+            new_line.append(mp_data[key])
+            output_data.append(new_line)
+    return output_data
 
 def getting_paths_mp(input_path):
     subfile_list = FM.get_file_paths(input_path)
@@ -49,17 +45,12 @@ def making_path_list(main_path, file_list): # maybe this should be in file_manag
 
 def parsing_matrix(path, deltimer):
     active_wells = []
-    with open(path) as csvfile:
-        reader = csv.reader(csvfile, delimiter)
-        num_row = 0
-        for row in reader:
-            num_column = 0
-            for column in row:
-                if column == "1":
-                    nums = [num_row, num_column]
-                    active_wells.append(nums)
-                num_column = num_column + 1
-            num_row = num_row + 1
+    data = FM.read_csv(path, deltimer)
+    for row in range(len(data)):
+        for col in range(len(data[row])):
+            if data[row][col] == "1":
+                nums = [row, col]
+                active_wells.append(nums)
     return active_wells
 
 def parsing_map_plate(mp_path, paths_mp, deltimer):
@@ -69,16 +60,16 @@ def parsing_map_plate(mp_path, paths_mp, deltimer):
     active_wells = parsing_matrix(active_path, deltimer)
     for well in active_wells:
         result = []
-        name = collecting_exp_settings(names_path, well, deltimer)
+        name = collecting_exp_settings(names_path, well, deltimer = ",")
         for mp_file in paths_mp:
             param = collecting_exp_settings(mp_file, well, deltimer)
             result.append(param)
         param_dict[name] = ",".join(result)
     return param_dict
   
-def collecting_exp_settings(mp_file, well, mark):
+def collecting_exp_settings(mp_file, well, deltimer):
     row_nr, col_nr = well
-    data = FM.read_csv(mp_file, mark)
+    data = FM.read_csv(mp_file, deltimer)
     return (data[row_nr][col_nr])
     
 def getting_column_titles(abs_path, paths):
@@ -92,12 +83,22 @@ def getting_column_titles(abs_path, paths):
     #names = ",".join(names)
     return names
 
-def combine(path_csv, path_map_plate, path_output, csv_names, deltimer = "\t"):
+def combine(path_csv, path_map_plate, path_output, csv_names, deltimer = "\t", csv_deltimer = ","):
     f_paths_map_plate = getting_paths_mp(path_map_plate)
     f_paths_output = making_path_list(path_output, csv_names)
-    output = parsing_map_plate(path_map_plate, f_paths_map_plate, deltimer)
+    if isinstance(path_csv,str):
+        f_paths_csv = making_path_list(path_csv, csv_names)
+    mp_output = parsing_map_plate(path_map_plate, f_paths_map_plate, deltimer)
     names = getting_column_titles(path_map_plate, f_paths_map_plate)
-    f_paths_csv = making_path_list(path_csv, csv_names)
-    for i in range(len(f_paths_csv)):
-        writing_output_csv(f_paths_csv[i], f_paths_output[i], output, names)
-
+    for i in range(len(f_paths_output)):
+        if isinstance(path_csv,dict):
+            in_data = paths_csv[csv_names[i]]  
+        elif isinstance(path_csv,str):
+            in_data = FM.read_csv(f_paths_csv[i], csv_deltimer)
+        else:
+            break #might be some error msg
+        if FM.if_exist(f_paths_output[i]):
+            FM.remove_directory(f_paths_output[i])
+        out = preparing_output_csv(in_data, f_paths_output[i], mp_output, names)
+        FM.write_csv(f_paths_output[i], deltimer, out)
+        
