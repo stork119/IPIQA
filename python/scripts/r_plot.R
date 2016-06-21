3ccb559465587a64e65587b4fef227f782ea890c
Setnea <gromadka.agnieszka@gmail.com>
2016-06-21 10:50:11 +0200

    Debug input_settings, remove r_plot script
    Debug input settings: remove parameter causing file overwriting
    Remove r_plot: removing non needed script

--- python/scripts/r_plot.R
+++
@@ -1,6 +0,0 @@
-f <- function(data, x_col_name, y_col_name , path){
-  library(ggplot2)
-
-  g <- ggplot(data, aes_string( x = x_col_name, y = y_col_name)) + geom_point()
-  ggsave(filename = path, plot = g)
-}
