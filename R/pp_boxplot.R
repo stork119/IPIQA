c2517434bc9559c993ced6093b3e759e057947bc
Setnea <gromadka.agnieszka@gmail.com>
2016-06-22 14:36:30 +0200

    [FIX] pp_boxplot.R
    Changing names of variables to:
    let them pass throw xml_parsing without troubles

--- R/pp_boxplot.R
+++ R/pp_boxplot.R
@@ -5,30 +5,29 @@
 ### ###
 
 # data - data.frame
-# filename - path to plot output file 
+# output_path - path to plot output file 
 # y - column with intensities "Nuclei.Intensity_MeanIntensity_CHA" or "Nuclei.Intensity_IntegratedIntensity_CHA"
 # x - "compare.1.1"
-# boxplot.group - "PositionName" or "ImageNumber"
-# x_axis.group - "group.?.?"
+# boxplot_group - "PositionName" or "ImageNumber"
+# x_axis_group - "group.?.?"
 
 library("ggplot2") ### ???
 
 # data <- read.csv(file = "E:/AG/PathwayPackage/resources/output/2016-06-07/raw/map_plate/Nuclei.csv", sep = "\t", header = TRUE)
-# filename <- "C:/Users/Pathway/Desktop/machlojki/plot.pdf"
+# output_path <- "C:/Users/Pathway/Desktop/machlojki/plot.pdf"
 # x <- "compare.1.1"
 # y <- "Intensity_MeanIntensity_CHA"
-# boxplot.group <-"PositionName"
-# x_axis.group <- "group.1.1"
-
+# boxplot_group <-"PositionName"
+# x_axis_group <- "group.1.1"
 
 pp.boxplot <- function(
   data,
-  filename,
+  output_path,
   x,
   y,
-  boxplot.group,
-  x_axis.group,
-  facet_grid.group = x_axis.group,
+  boxplot_group,
+  x_axis_group,
+  facet_grid.group = x_axis_group,
   ylab = y,
   xlab = x,
   ylim.min = 0,
@@ -45,8 +44,8 @@ pp.boxplot <- function(
   gplot <- ggplot(data = data, 
                   aes_string(x = x,
                              y = y,
-                             group = boxplot.group),
-                  group = x_axis.group
+                             group = boxplot_group),
+                  group = x_axis_group
   ) + 
     do.call(plot_fun, args = list()) +
     ylim(ylim.min, ylim.max) +
@@ -56,7 +55,7 @@ pp.boxplot <- function(
     theme(legend.position = "bottom",
           axis.text.x = element_text(angle = xlab.angle,
                                      hjust = xlab.hjust))
-  ggsave(filename = filename,
+  ggsave(filename = output_path,
          plot = gplot,
          width = plot.width,
          height = plot.height,
