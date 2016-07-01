3d70cde54f6bdc24608ebcb6b9fc515b210760d4
Setnea <gromadka.agnieszka@gmail.com>
2016-07-01 13:23:35 +0200

    Adding 2 R scirpts

---
+++ R/plot_density.R
@@ -0,0 +1,57 @@
+### ###
+###
+### plot_density
+###
+### ###
+
+#### PREPROCESSING  ####
+
+### libraries ###
+try({package.list <- list("ggplot2")
+     package.load <- sapply(package.list, function(package.name){
+       package.exist <- require(package.name, character.only = TRUE)
+       if(!package.exist){
+         install.packages(package.name)
+         return(library(package.name, character.only = TRUE))
+       }
+       return(package.exist)
+     })
+})
+
+### sources ###
+#wd.tmp <- "X:/EG/CellProfiller/Analysis/2016-06-16-pstat1-summary/"
+source(paste(wd.tmp, "theme_jetka.R", sep = ""))
+
+
+#### MAIN ####
+plot_density <- function(data,
+                         filename,
+                         x = "Intensity_IntegratedIntensity_DAPI",
+                         data_nrow_min = 10,
+                         ylab = "density",
+                         xlab = x,
+                         xlim.min = 0,
+                         xlim.max = max(data[,x]),
+                         bin_number = 50,
+                         bin_width = (xlim.max - xlim.min)/bin_number,
+                         plot.width = 24,
+                         plot.height = 8,
+                         plot.title = "",
+                         theme.text_size = 12,
+                         line.size = 1.5){
+  
+  if(nrow(data) > data_nrow_min){  
+    gplot <- ggplot(data = data) +
+      geom_density(aes_string(x = x, "..density.."), line.size = line.size) +
+      xlim(xlim.min, xlim.max) + 
+      ggtitle(plot.title) +
+      theme_jetka(text_size = theme.text_size)
+    ggsave(filename = paste(filename, ".pdf", sep = ""),
+           plot = gplot,
+           width = plot.width,
+           height = plot.height,
+           useDingbats = FALSE)
+    return(gplot)
+  }
+  return()
+}
