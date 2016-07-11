8a6ca24dece4398184bd1776b18161983ade83f5
Setnea <gromadka.agnieszka@gmail.com>
2016-07-11 15:03:36 +0200

    Editing WorkflowEngine
    Adding option for processing all settings from given directory

--- python/WorkflowEngine.py
+++ python/WorkflowEngine.py
@@ -18,15 +18,26 @@ def main():
                 type = str, 
                 required = True, 
                 nargs = 1, 
-                help = 'Path to file with input_settings (in XML format).')
+                help = 'Path to input_settings (in XML format).')
+    parser.add_argument('-m',
+                metavar = '<multiplesettings>', 
+                type = str, 
+                default=False,
+                nargs = 1, 
+                help = 'Option allows to process multiple configuration settings.')
     args = parser.parse_args()
     """Setting up pipeline."""
     logger.debug("Input settings source: %s", args.s[0])
-    if os.path.isabs(args.s[0]):
-        input_settings = FM.path_unify(args.s[0])
+    if os.path.isabs(args.s[0]): #relative or absolute path
+        settings_path = FM.path_unify(args.s[0])
     else:
-        input_settings = FM.path_join(PP_path, "configuration_settings" , args.s[0])
-    pipeline, config_dict = XML_P.parse(input_settings)
-    pipeline.execute(config_dict)
+        settings_path = FM.path_join(PP_path, "configuration_settings" , args.s[0])
+    if args.m != False: #single or multiple settings
+        settings_list = FM.file_get_paths(settings_path)
+    else:
+        settings_list = [settings_path]
+    for setts in settings_list:
+        pipeline, config_dict = XML_P.parse(setts)
+        pipeline.execute(config_dict)
 if __name__ == "__main__":
     main()
