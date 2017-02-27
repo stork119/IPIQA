0f652c3a45a55879d5a1ea55fffd0ca15e906541
Agnieszka <Agnieszka>
2017-02-27 10:58:15 +0100

    Adding additional functions for map_plate #64

--- python/modules/map_plate.py
+++ python/modules/map_plate.py
@@ -86,3 +86,39 @@ def _collect_exp_settings(abs_path, mp_file, delimiter, mp_dict):
         value = (data[position[0]][position[1]])
         mp_dict[well][name] = value
 
+def get_param_value(mp_dict, well_name, param): # get values for a given well and parameter
+    value = mp_dict[well_name][param]
+    return value
+
+def get_param_all_values(mp_dict, param): # get all values for a given param from map_plate dictionary
+    values = []
+    for well in mp_dict:
+        values.append(mp_dict[well][param])
+    return values
+
+def get_param_unique_values(mp_dict, param): # get all unique values for a given param from map_plate dictionary
+    values = get_param_all_values(mp_dict, param)
+    unique = list(set(values))
+    return unique
+
+def get_all_params_names(mp_dict): # get all key names from map_plate dictionary
+    names = []
+    key_0 = list(mp_dict.keys())[0]
+    names = list(mp_dict[key_0].keys()) # if we assume that number of information between wells is constant
+    """params_count = len(mp_dict[key_0])
+    for well in mp_dict: # if we assume that number of information about wells may somehow differ
+        tmp= len(mp_dict[well])
+        if params_count != tmp:
+            logger.warning("Some wells are not described by the same number of information: %s, %s", key_0, well)
+            if tmp > params_count:
+                names = []
+                params_count = tmp
+                for param in mp_dict[well]:
+                    names.append(param)"""
+    return names
+
+def get_well_params(mp_dict, well): # get all parameters values for a given well
+    values = []
+    for param in mp_dict[well]:
+        values.append(mp_dict[well][param])
+    return values
