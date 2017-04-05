4ab83223a03e870db697cd48993a6c09d311cc0c
Agnieszka <gromadka.agnieszka@gmail.com>
2017-04-05 15:17:14 +0200

    VariableMP #130 part I
    Creating placeholder for new class

--- python/modules/variable.py
+++ python/modules/variable.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 import logging, sys
+import modules.file_managment as FM
 
 logger = logging.getLogger("Variable module")
 
@@ -18,6 +19,9 @@ class Variable():
     def set_value(self, value, args = {}):
         self.value = value
         return
+
+    def get_args(self):
+        return self.args
 
 class VariableReference(Variable):
     def __init__(self, key, value, args = {}):
@@ -51,7 +55,19 @@ class VariableParted(Variable):
         for key in order:
             v_part = self.value[key].get_value(env)
             values_list.append(v_part)
-        return "".join(values_list)
+        contains_path = False
+        for key, variable in self.value.items():
+            try:
+                p_type = variable.get_args()["type"]
+                if p_type == "path":
+                    contains_path = True
+            except:
+                pass
+        if contains_path:
+            merged_value = FM.path_join(*values_list)
+        else:
+            merged_value = "".join(values_list)
+        return merged_value
 
 class VariableList(Variable):
     def __init__(self, key, value, args = {}):
@@ -68,4 +84,12 @@ class VariableList(Variable):
             out_list.append(element.get_value(env))
         return out_list
 
+class VariableMP(Variable):
+    def __init__(self, key, value, args = {}):
+        Variable.__init__(self, key, value, args = {})
 
+    def get_variable(self, env):
+        pass
+
+    def get_value(self, env, args = {}):
+        pass
