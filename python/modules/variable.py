34a82ce9575233554be205d87f61f9e44dc1db3a
Agnieszka <gromadka.agnieszka@gmail.com>
2017-04-10 13:30:11 +0200

    Functions for VariablePath #138

--- python/modules/variable.py
+++ python/modules/variable.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 import logging, sys
 import copy
+import modules.file_managment as FM
 
 logger = logging.getLogger("Variable module")
 
@@ -43,21 +44,37 @@ class VariableParted(Variable):
     def __init__(self, key, value, args = {}):
         Variable.__init__(self, key, value, args = {})
 
-    def get_variable(self, env):
-        # creates variable with merged value
-        merged_value = self.get_value(env)
-        var = Variable(self.key, merged_value)
-        return var
+    def check_paths_presence(self, var_dict):
+        for key, variable in var_dict.items():
+            if isinstance(variable, VariablePath):
+                return True
+        return False
 
     def get_value(self, env, args = {}):
+        return self._get_value(env, args = {})[0]
+
+    def get_variable(self, env):
+        # creates variable with merged value
+        merged_value, contains_path = self._get_value(env)
+        if contains_path:
+            var = VariablePath(self.key, merged_value)
+        else:
+            var = Variable(self.key, merged_value)
+        return var
+
+    def _get_value(self, env, args = {}):
         order = sorted(self.value.keys())
         values_list = []
         for key in order:
             v_part = self.value[key].get_value(env)
             values_list.append(v_part)
-        merged_value = "".join(values_list)
+        contains_path = self.check_paths_presence(self.value)
+        if contains_path:
+            merged_value = FM.path_join(*values_list)
+        else:
+            merged_value = "".join(values_list)  
         logger.debug("Merged variable %s: %s", self.key, merged_value)
-        return merged_value
+        return merged_value, contains_path
 
 class VariableList(Variable):
     """
@@ -87,4 +104,3 @@ class VariableList(Variable):
         for element in self.value:
             out_list.append(element.get_value(env))
         return out_list
-
